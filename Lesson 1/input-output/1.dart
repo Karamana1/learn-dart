be7ef1d7d92fc307cbb9f3d2e21a459c2431b6df
bro stk <brostk962@gmail.com>
2025-01-18 12:54:48 +0700

    Your commit message

--- Lesson 1/input-output/1.dart
+++ Lesson 1/input-output/1.dart
@@ -1,10 +1,54 @@
 import 'dart:io';
 
 void main() {
-  print("Hello Flutter");
+  int? id;
   String? name;
-  print("Enter Name : ");
-  name = stdin.readLineSync()!;
+  String? Sex;
+  String? Address;
+  print("=============================== input ==============================");
+  stdout.write("\tEnter your ID     : ");
+  id = int.parse(stdin.readLineSync()!);
 
-  print("Name : " + name);
+  stdout.write("\tEnter your Name   : ");
+  name = stdin.readLineSync();
+
+  stdout.write("\tEnter your Sex    : ");
+  Sex = stdin.readLineSync();
+
+  stdout.write("\tEnter your Age    : ");
+  int? Age;
+  Age = int.parse(stdin.readLineSync()!);
+
+  stdout.write("\tEnter your Address: ");
+  Address = stdin.readLineSync();
+
+  int? score;
+  stdout.write("Enter Score : ");
+  score = int.parse(stdin.readLineSync()!);
+
+  print(
+      '\n ============================== ouput ==============================');
+  print("\tID     : $id");
+  print("\tName   : $name");
+  print("\tAge    : $Age");
+  print("\tSex    : $Sex");
+  print("\tAddress: $Address");
+  print(
+      '\n ===================================================================');
+
+  if (Age <= 40) {
+    print("Hello Mr.$name");
+  } else if (Age > 40) {
+    print("Hello Uncle.$name");
+  } else {
+    print("Hi Mrs.$name");
+  }
+  String grade = (score >= 90)
+      ? "A"
+      : (score >= 80)
+          ? "B"
+          : (score >= 70)
+              ? "C"
+              : "F";
+  print('Grade : $grade');
 }
---
+++ Lesson 1/loop/Foreach.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+
+void main() {
+  List name = ["A", "B", "C", "D", "E", "F"];
+  for (var i in name) {
+    print(i);
+  }
+}
